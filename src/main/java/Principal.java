1fbb51b6252ea8e14ba6049b4a71f75fe4682b16
Axel Garcia <30581844+pacisauctor@users.noreply.github.com>
2022-11-10 15:49:31 -0600

    add comment lol

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -2,6 +2,7 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("My name is: Axel Garcia");
+        // lol
     }
 }
 
